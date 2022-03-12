976c96498518d6cb7354077250c770c3e7ccc203
Teresiah-cpu <terrywambo2018@gmail.com>
2022-03-12 16:48:42 +0300

    add the jquery toggle in the what we do section

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $("#pdesign").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#button2").click(function(){
+    $("#pdev").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#button3").click(function(){
+    $("#pproduct").toggle();
+  });
+});
